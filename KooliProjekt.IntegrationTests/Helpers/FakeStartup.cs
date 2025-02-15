4c97f1ee0d819a27bef1635e6d9da8363b6a9cbc
Gunnar Peipman <gpeipman@hotmail.com>
2025-02-15 12:43:20 +0200

    lisatud integratsioonitestide projekt

---
+++ KooliProjekt.IntegrationTests/Helpers/FakeStartup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using KooliProjekt.Controllers;
+using KooliProjekt.Data;
+using KooliProjekt.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace KooliProjekt.IntegrationTests.Helpers
+{
+    public class FakeStartup //: Startup
+    {
+        public FakeStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+            var dbGuid = Guid.NewGuid().ToString();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("TestConnection"));
+            });
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddControllersWithViews()
+                    .AddApplicationPart(typeof(HomeController).Assembly);
+
+
+            services.AddScoped<ITodoListService, TodoListService>();    
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}/{pathStr?}");
+            });
+
+            var serviceScopeFactory = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>();
+            using (var serviceScope = serviceScopeFactory.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+                if (dbContext == null)
+                {
+                    throw new NullReferenceException("Cannot get instance of dbContext");
+                }
+
+                var connectionString = dbContext.Database.GetDbConnection().ConnectionString;
+                if (!connectionString.Contains("aspnet-KooliProjekt-Integration"))
+                {
+                    throw new Exception("LIVE SETTINGS IN TESTS!");
+                }
+
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+            }
+        }
+    }
+}
---
+++ KooliProjekt.IntegrationTests/HomeControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using KooliProjekt.IntegrationTests.Helpers;
+using Xunit;
+
+namespace KooliProjekt.IntegrationTests
+{
+    [Collection("Sequential")]
+    public class HomeControllerTests : TestBase
+    {
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Home/Privacy")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = Factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+
+        //[Fact]
+
+        //public async Task Get_AnonymousCanAccessPrivacy()
+        //{
+        //    // Arrange
+        //    using var client = Factory.CreateClient();
+
+        //    // Act
+        //    using var response = await client.GetAsync("/Home/Privacy");
+
+        //    // Assert
+        //    response.EnsureSuccessStatusCode();
+        //    Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        //}
+    }
+}
